e6178f48ff6535fae1568a14e369c13f686c9e0e
MrMo5235 <2307310687@qq.com>
2024-09-01 19:55:00 +0800

    Add files via upload

---
+++ stm32/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"                  // Device header
+#include "LED.h"
+#include "Delay.h"
+#include "Key.h"
+
+uint8_t KeyNum;
+
+int main(void)
+{
+  LED_Init();
+	Key_Init();
+	
+	while(1)
+	{
+		KeyNum = Key_GetNum();
+		if (KeyNum == 1)
+		{
+			LED1_turn();
+		}
+		if (KeyNum == 2)
+		{
+			LED2_turn();
+		}
+	}
+}
